dfea25795b01698e8e9450bc7ed0716d95910db8
yilan505 <185388706+yilan505@users.noreply.github.com>
2026-09-20 17:00:12 +0800

    Update plugins: luci-app-momo, momo, luci-app-wolplus

--- luci-app-momo/htdocs/luci-static/resources/view/momo/log.js
+++ luci-app-momo/htdocs/luci-static/resources/view/momo/log.js
@@ -24,7 +24,34 @@ return view.extend({
 
         m = new form.Map('momo');
 
-        s = m.section(form.NamedSection, 'placeholder', 'placeholder', _('Log'));
+        s = m.section(form.NamedSection, 'log', 'log', _('Log'));
+
+        s.tab('log_config', _('Log Config'));
+        
+        o = s.taboption('log_config', form.Flag, 'clear_at_stop', _('Clear At Stop'));
+        o.rmempty = false;
+        
+        o = s.taboption('log_config', form.Flag, 'scheduled_clear', _('Scheduled Clear'));
+        o.rmempty = false;
+        
+        o = s.taboption('log_config', form.Value, 'scheduled_clear_cron', _('Scheduled Clear Cron'));
+        o.retain = true;
+        o.rmempty = false;
+        o.depends('scheduled_clear', '1');
+
+        o = s.taboption('log_config', form.Value, 'scheduled_clear_size_limit', _('Scheduled Clear Size Limit'));
+        o.retain = true;
+        o.rmempty = false;
+        o.datatype = 'uinteger';
+        o.depends('scheduled_clear', '1');
+
+        o = s.taboption('log_config', form.ListValue, 'scheduled_clear_size_limit_unit', _('Scheduled Clear Size Limit Unit'));
+        o.retain = true;
+        o.rmempty = false;
+        o.depends('scheduled_clear', '1');
+        o.value('KB', 'KB');
+        o.value('MB', 'MB');
+        o.value('GB', 'GB');
 
         s.tab('app_log', _('App Log'));
 
@@ -119,8 +146,5 @@ return view.extend({
         };
 
         return m.render();
-    },
-    handleSaveApply: null,
-    handleSave: null,
-    handleReset: null
+    }
 });
--- momo/files/uci-defaults/migrate.sh
+++ momo/files/uci-defaults/migrate.sh
@@ -21,6 +21,39 @@ procd=$(uci -q get momo.procd); [ -z "$procd" ] && {
 	uci del momo.config.fast_reload
 }
 
+# since v1.1.1
+
+dummy_device=$(uci -q get momo.routing.dummy_device); [ -z "$dummy_device" ] && uci set momo.routing.dummy_device=momo-dummy
+
+# since v1.1.2
+
+config_scheduled_restart_cron=$(uci -q get momo.config.scheduled_restart_cron); [ -z "$config_scheduled_restart_cron" ] && uci rename momo.config.cron_expression="scheduled_restart_cron"
+
+section_log=$(uci -q get momo.log); [ -z "$section_log" ] && {
+	uci set momo.log=log
+	uci set momo.log.scheduled_clear=1
+	uci set momo.log.scheduled_clear_cron="*/5 * * * *"
+	uci set momo.log.scheduled_clear_size_limit=1
+	uci set momo.log.scheduled_clear_size_limit_unit=MB
+}
+
+section_mixin=$(uci -q get momo.mixin); [ -z "$section_mixin" ] && {
+	uci set momo.mixin=mixin
+	uci set momo.mixin.log_disabled='0'
+	uci set momo.mixin.log_level='info'
+	uci set momo.mixin.log_timestamp='1'
+	uci set momo.mixin.dns_independent_cache='1'
+	uci set momo.mixin.dns_reverse_mapping='1'
+	uci set momo.mixin.cache_enabled='1'
+	uci set momo.mixin.cache_store_fakeip='1'
+	uci set momo.mixin.cache_store_rdrc='1'
+	uci set momo.mixin.external_control_ui_path='ui'
+	uci set momo.mixin.external_control_ui_download_url='https://github.com/Zephyruso/zashboard/releases/latest/download/dist-cdn-fonts.zip'
+}
+
+# since v1.2.1
+config_clear_at_stop=$(uci -q get momo.log.clear_at_stop); [ -z "$config_clear_at_stop" ] && uci set momo.log.clear_at_stop=1
+
 # commit
 uci commit momo
 
